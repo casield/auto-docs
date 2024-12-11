8e8ec6a2c639743cac251ce7491cfb7a9778fb39
Casiel <casield@gmail.com>
2024-12-11 00:02:43 -0600

    chore: build

--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -5,7 +5,6 @@ class ServerlessPlugin {
   options: any;
   utils: any;
 
-  commands: {};
   hooks: { [key: string]: Function };
 
   constructor(serverless: Serverless, options: any, utils: any) {
@@ -13,44 +12,21 @@ class ServerlessPlugin {
     this.options = options;
     this.utils = utils;
 
-    this.commands = {
-      welcome: {
-        usage: "Helps you start your first Serverless plugin",
-        lifecycleEvents: ["hello", "world"],
-        options: {
-          message: {
-            usage:
-              "Specify the message you want to deploy " +
-              "(e.g. \"--message 'My Message'\" or \"-m 'My Message'\")",
-            required: true,
-            shortcut: "m",
-          },
-        },
-      },
-    };
-
     this.hooks = {
-      "before:welcome:hello": this.beforeWelcome.bind(this),
-      "welcome:hello": this.welcomeUser.bind(this),
-      "welcome:world": this.displayHelloMessage.bind(this),
-      "after:welcome:world": this.afterHelloWorld.bind(this),
+      initialize: () => this.init(),
+      "before:deploy:deploy": () => this.beforeDeploy(),
+      "after:deploy:deploy": () => this.afterDeploy(),
     };
   }
 
-  beforeWelcome() {
-    this.utils.log.info("Hello from Serverless!");
+  init() {
+    // Initialization
   }
-
-  welcomeUser() {
-    this.utils.log.info("Your message:");
+  beforeDeploy() {
+    // Before deploy
   }
-
-  displayHelloMessage() {
-    this.utils.log.info(`${this.options.message}`);
-  }
-
-  afterHelloWorld() {
-    this.utils.log.info("Please come again!");
+  afterDeploy() {
+    // After deploy
   }
 }
 
