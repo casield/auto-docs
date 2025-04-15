6c0ab696bf0fbd4d0415d7b376f7dbb740ccae81
Casiel <casield@gmail.com>
2025-04-14 19:14:44 -0600

    test: add codeAnalyzer test again

--- packages/core/src/__test__/analyzer/CodeAnalyzer.test.ts
+++ packages/core/src/__test__/analyzer/CodeAnalyzer.test.ts
@@ -1,79 +1,79 @@
-// import {
-//   CodeAnalyzer,
-//   AnalyzerOptions,
-//   ReturnAnalysis,
-// } from "../../analyzer/CodeAnalyzer";
-// import { parseComment } from "../../utils";
+import {
+  CodeAnalyzer,
+  AnalyzerOptions,
+  ReturnAnalysis,
+} from "../../analyzer/CodeAnalyzer";
+import { parseComment } from "../../utils";
 
-// describe("CodeAnalyzer", () => {
-//   const options: AnalyzerOptions = {};
+describe("CodeAnalyzer", () => {
+  const options: AnalyzerOptions = {};
 
-//   it("should analyze source code and return correct return statements and class methods", () => {
-//     const sourceCode = `
-//     import { myFunction } from "../../final-framework/main.ts";
+  it("should analyze source code and return correct return statements and class methods", () => {
+    const sourceCode = `
+    import { myFunction } from "../../final-framework/main.ts";
 
-//     export const testFunction = () => {
-//       if(true) {
-//         return "Hello World";
-//       }
+    export const testFunction = () => {
+      if(true) {
+        return "Hello World";
+      }
 
-//       // This is a comment
-//       return myFunction();
-//     }
+      // This is a comment
+      return myFunction();
+    }
 
-//     export class TestClass {
-//       public testMethod() {
-//         return myFunction();
-//       }
-//     }
+    export class TestClass {
+      public testMethod() {
+        return myFunction();
+      }
+    }
 
-//     export function testFunction2() {
-//       return myFunction();
-//     }
+    export function testFunction2() {
+      return myFunction();
+    }
 
-//     export const testFunction3 = () => myFunction();
+    export const testFunction3 = () => myFunction();
 
-//     `;
+    `;
 
-//     const analyzer = new CodeAnalyzer("testFile.ts", options);
-//     const analysis: ReturnAnalysis = analyzer.analyzeSource(sourceCode);
+    const analyzer = new CodeAnalyzer("testFile.ts", options);
+    const analysis: ReturnAnalysis = analyzer.analyzeSource(sourceCode);
 
-//     expect(analysis).toBeDefined();
-//     expect(analysis.functions.testFunction.returnStatements.length).toBe(2);
-//     expect(analysis.functions.testFunction.returnStatements[0].value).toBe(
-//       "myFunction()"
-//     );
+    expect(analysis).toBeDefined();
+    expect(analysis.functions.testFunction.returnStatements.length).toBe(2);
+    expect(analysis.functions.testFunction.returnStatements[0].value).toBe(
+      "myFunction()"
+    );
 
-//     expect(
-//       analysis.functions["TestClass.testMethod"].returnStatements.length
-//     ).toBeDefined();
-//   });
+    expect(
+      analysis.functions["TestClass.testMethod"].returnStatements.length
+    ).toBeDefined();
+  });
 
-//   it("should get the return comments", () => {
-//     const sourceCode = `
-//     import { myFunction } from "../../final-framework/main.ts";
+  it("should get the return comments", () => {
+    const sourceCode = `
+    import { myFunction } from "../../final-framework/main.ts";
 
-//     /**
-//      * @auto-docs
-//      * Hello this is a comment
-//      */
-//     export const testFunction = () => {
-//       // This is a comment
-//       return {
-//         hello: "world"
-//       }
-//     }
+    /**
+     * @auto-docs
+     * Hello this is a comment
+     */
+    export const testFunction = () => {
+      // This is a comment
+      return {
+        hello: "world"
+      }
+    }
 
-//     `;
+    `;
 
-//     const analyzer = new CodeAnalyzer("testFile.ts", options);
-//     const analysis: ReturnAnalysis = analyzer.analyzeSource(sourceCode);
+    const analyzer = new CodeAnalyzer("testFile.ts", options);
+    const analysis: ReturnAnalysis = analyzer.analyzeSource(sourceCode);
 
-//     expect(analysis.functions.testFunction.returnStatements[0].comment).toBe(
-//       "This is a comment"
-//     );
+    expect(analysis.functions.testFunction.returnStatements[0].comment).toBe(
+      "This is a comment"
+    );
 
-//     const comment = parseComment(analysis.functions.testFunction.comment || "");
-//     expect(comment?.comment).toBe("Hello this is a comment");
-//   });
-// });
+    const comment = parseComment(analysis.functions.testFunction.comment || "");
+    expect(comment?.comment).toBe("Hello this is a comment");
+  });
+});
