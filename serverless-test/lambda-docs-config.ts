b224b2082184ad632dd6283e51724bd8c8501ced
Casiel <casield@gmail.com>
2025-04-30 23:16:05 -0600

    fix: working outside package

--- serverless-test/lambda-docs-config.ts
+++ serverless-test/lambda-docs-config.ts
@@ -1,10 +1,14 @@
 import { AutoDocsBuilder, MemoryLinker } from "@auto-docs/core";
+import { OpenApiDoc } from "@auto-docs/openapi-plugin";
+import { DynamicProxyLinker } from "@auto-docs/serverless-dynamic";
 
 const builder = new AutoDocsBuilder({
   name: "My Custom Docs",
   description: "Documentation generated with a custom builder",
-  plugins: [],
-  linker: new MemoryLinker(),
+  plugins: [OpenApiDoc],
+  linker: new DynamicProxyLinker(
+    "https://7lqn0bxcch.execute-api.us-east-1.amazonaws.com/proxy"
+  ),
   branch: "main",
 });
 
